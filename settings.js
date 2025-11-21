89539a67ff738bc6679403d00e321fd6f2754bea
ryanreny243-source <ryanreny243@gmail.com>
2025-11-21 14:47:00 +0300

    Update settings.js3

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
+  packname: 'Ray Bot',
+  author: 'Ryan ray‎',
+  botName: "Rayn Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '254757829372', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
