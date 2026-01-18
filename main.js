660ed3fa0589c6cbb1dd1c7027aab050f400a9cf
voldigoa46-del <voldigoa46@gmail.com>
2026-01-18 23:11:09 +0000

    Update main.js

--- main.js
+++ main.js
@@ -120,8 +120,8 @@ const soraCommand = require('./commands/sora');
 // Global settings
 global.packname = settings.packname;
 global.author = settings.author;
-global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A";
-global.ytch = "Mr Unique Hacker";
+global.channelLink = "https://whatsapp.com/channel/0029VbBmVyUFsn0WofgxwO0Z";
+global.ytch = "Christus";
 
 // Add this near the top of main.js with other global configurations
 const channelInfo = {
